25e90e1762358cffc624dbd3aaafb31179a0dc3f
TepNik <tepelin.nikita@gmail.com>
2020-12-30 11:30:59 +0300

    Fixes

--- Main.java
+++ Main.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Scanner;
+import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;
 
 
 public class Main {
